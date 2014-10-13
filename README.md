84bdf6a2215ecc457df2cb57e71a5ca754c00b46
Kaushal Kishore <kaushal@osscube.com>
2014-10-13 12:22:32 +0530

    Cruisecontrol Docker Setup

---
+++ README.md
@@ -0,0 +1,4 @@
+Docker-CruiseControl
+====================
+
+Dockerfile to build Cruise Control Installed Containers
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Executing supervisord and mysql_user
+# Author : Kaushal Kishore <kaushal.rahuljaiswal@gmail.com>
+
+exec supervisord -n
+
